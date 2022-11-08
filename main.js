65fe0970d50c166942ba6b83351ce03fe87e3749
Chris Kim <ckim@havenagency.com>
2022-11-08 14:11:22 -0800

    update lottie file

--- main.js
+++ main.js
@@ -1,27 +1,12 @@
 const lottieContainer = document.getElementById('container');
-const lottieContainer2 = document.getElementById('container2');
-
-const balloonIntroSequence = bodymovin.loadAnimation({
-    container: lottieContainer,
-    renderer: 'svg',
-    loop: false,
-    path: '/lottie/22-1026_Coachella_balloon-line--intro-sequence-1440.json',
-    autoplay: true,
-    name: 'Intro Sequence',
-});
 
 const balloonFullSequence = bodymovin.loadAnimation({
-    container: lottieContainer2,
-    path: '/lottie/22-1026_Coachella_balloon-line--full-sequence-1440.json',
+    container: lottieContainer,
+    path: '/lottie/22-1108_Coachella_balloon-line.json',
     renderer: 'svg',
     loop: false,
     autoplay: true,
     name: 'Full Sequence',
-});
-
-balloonIntroSequence.addEventListener('complete', () => {
-    lottieContainer.classList.add('hide');
-    lottieContainer2.classList.remove('hide');
 });
 
 function animatebodymovin(duration) {
@@ -33,8 +18,7 @@ function animatebodymovin(duration) {
     balloonFullSequence.goToAndStop(frame, true);
 }
 const onScroll = () => {
-    console.log('scrolling');
-    animatebodymovin(1000);
+    animatebodymovin(2000);
 };
 
 document.addEventListener('scroll', onScroll);
